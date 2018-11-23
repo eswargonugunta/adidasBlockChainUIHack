8218cfaf622a50ce2226ba4dc75bceaac61baf48
Gonugunta Eswar Prasad <gonugunta_prasad@infosys.com>
2018-11-23 23:59:36 +0530

    designer page

---
+++ public/components/adidasDesignerHome/adidasDesignerHome.js
@@ -0,0 +1,103 @@
+'use strict';
+
+app.adidasDesignerHome = kendo.observable({
+    onShow: function () { },
+    afterShow: function () { }
+});
+app.localization.registerView('adidasDesignerHome');
+
+(function (parent) {
+    var
+        homeModel = kendo.observable({
+            logout: function (e) {
+                app.logout();
+            }
+            
+        });
+
+    parent.set('homeModel', homeModel);     
+    parent.set('onShow', function (e) {
+        localStorage.clear();
+
+        var template = kendo.template($("#cartlistTemplate").html());
+
+        var data = [
+            {
+                "cartItem": "MEN'S ADIDAS RUNNING PUREBOOST RBL SHOES",
+                "cartId":1,
+                "img": "img/cart/shoes1.jpg",
+                "saleType":1, "itemVerified":1,
+                "manufacturedate":"02-Jan-2018","price":"$100"
+            },
+            {
+                "cartItem": "ADIDAS GOLETTO VI FG FOOTBALL SHOES FOR MEN",
+                "cartId":2,
+                "img": "img/cart/shoes2.jpg",
+                "saleType":1, "itemVerified":1,
+                "manufacturedate":"10-Jan-2018","price":"$120"
+            },
+            {
+                "cartItem": "ADIDAS NEMEZIZ MESSI 18.4 FXG FOOTBALL SHOES FOR MEN",
+                "cartId":3,
+                "img": "img/cart/shoes3.jpg",
+                "saleType":2, "itemVerified":2,
+                "manufacturedate":"21-Feb-2012","price":"$200"
+            },
+            {
+                "cartItem": "ADIDAS ACE 16.4 FXG J SOCCER FOR MEN",
+                "cartId":1,
+                "img": "img/cart/shoes4.jpg",
+                "saleType":1, "itemVerified":1,
+                "manufacturedate":"28-Dec-2016","price":"$150"
+            },
+            {
+                "cartItem": "ADIDAS X 16.1 FG FOOTBALL SHOES FOR MEN",
+                "cartId":1,
+                "img": "img/cart/shoes5.jpg",
+                "saleType":2, "itemVerified":2,
+                "manufacturedate":"15-Apr-2018","price":"$50"
+            },
+            {
+                "cartItem": "ADIDAS X TANGO 18.3 TF FOOTBALL SHOES FOR MEN",
+                "cartId":1,
+                "img": "img/cart/shoes6.jpg",
+                "saleType":1, "itemVerified":1,
+                "manufacturedate":"12-Mar-2014","price":"$130"
+            }
+        ];
+
+        var tempData = {
+            data: data
+        }
+        var result = template(tempData);
+        $("#cartlist").html(result);
+
+        /* // Step show event
+        $("#smartwizard").on("showStep", function(e, anchorObject, stepNumber, stepDirection, stepPosition) {
+            if(stepPosition === 'first'){
+                $("#prev-btn").addClass('invisible disabled');
+            
+            }else if(stepPosition === 'final'){
+                $("#next-btn").addClass('invisible disabled');
+                
+            }else{
+                $("#prev-btn").removeClass('invisible disabled');
+                $("#next-btn").removeClass('invisible disabled');
+            }
+         });
+         
+         
+         $('#smartwizard').smartWizard({
+                 selected: 0,
+                 theme: 'circles',
+                 transitionEffect:'fade',
+                 showStepURLhash: false,
+                 toolbarSettings: {toolbarPosition: 'bottom'}
+         }); */
+       
+    });
+
+    parent.set('afterShow', function (e) {
+        
+    });
+})(app.adidasDesignerHome);
